ed8f9e9e168b7837e2e3165eed71e6d6ad923e59
ManiaciNahuel <nahuelmaniaci@gmail.com>
2025-04-14 10:09:20 -0300

    seq ult actualizacion

--- models/productoUbicacion.js
+++ models/productoUbicacion.js
@@ -1,43 +1,47 @@
 // models/productoUbicacion.js
 module.exports = (sequelize, DataTypes) => {
-    const ProductoUbicacion = sequelize.define('ProductoUbicacion', {
-      codebar: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      tipo: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      numero: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      subdivision: {
-        type: DataTypes.STRING
-      },
-      cantidad: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      ubicacion: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      sucursalId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      fechaRegistro: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-      },
-      numeroSubdivision: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    });
-  
-    return ProductoUbicacion;
-  };
-  
+  const ProductoUbicacion = sequelize.define('ProductoUbicacion', {
+    codebar: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    tipo: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    numero: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    subdivision: {
+      type: DataTypes.STRING
+    },
+    cantidad: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    ubicacion: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    sucursalId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    fechaRegistro: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    numeroSubdivision: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    fechaActualizacion: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+
+  });
+
+  return ProductoUbicacion;
+};
